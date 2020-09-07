6938797a47e75d8bd86886b88943febb288507ae
Alexander <eyescreamxd@me.com>
2020-09-08 00:03:33 +0300

    Чуть-чуть ещё допилил бота

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -20,18 +20,9 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
-)
+                    )
 
 
-
-
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn', 
-        'password': 'python'
-    }
-}
 def check_planet(bot, update):
     planet = str(update.message.text.split(' ')[1]).lower()
     if planet == 'mercury':
@@ -59,22 +50,22 @@ def greet_user(bot, update):
 
 
 def talk_to_me(bot, update):
-    user_text = update.message.text 
+    user_text = update.message.text
     print(user_text)
     update.message.reply_text(user_text)
- 
+
 
 def main():
     mybot = Updater(API_KEY)
-    
+
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("planet", check_planet))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    
+
     mybot.start_polling()
     mybot.idle()
-       
+
 
 if __name__ == "__main__":
     main()
